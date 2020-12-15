810d8232d9f91fb4bead09e1acb60a15ad7ff9ee
Hynn <hynn@live.jp>
2020-12-15 18:10:01 -0300

    Alterando a Index - Modificando o menu

--- components/Menu.vue
+++ components/Menu.vue
@@ -1,12 +1,29 @@
 <template>
-	<div class="dropdown">
-		<button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-			Dropdown
-		</button>
-		<div class="dropdown-menu" aria-labelledby="dropdownMenu2">
-			<button class="dropdown-item" type="button">Action</button>
-			<button class="dropdown-item" type="button">Another action</button>
-			<button class="dropdown-item" type="button">Something else here</button>
+	<div class="flex flex-col">
+		<div class="dropdown">
+			<button class="btn btn-secondary btn-verde dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+				Portfólio
+			</button>
+			<div class="dropdown-menu text-center" aria-labelledby="dropdownMenu2">
+				<button class="dropdown-item" type="button">Layouts</button>
+				<button class="dropdown-item" type="button">Banners</button>
+				<button class="dropdown-item" type="button">Logos</button>
+			</div>
 		</div>
+
+		<a href="#" class="btn btn-secondary btn-verde">Sobre mim</a>
+		<a href="#" class="btn btn-secondary btn-verde">Contato</a>
 	</div>
 </template>
+
+<style>
+.btn-verde
+{
+	background-color: #88FF00;
+}
+
+.dropdown-menu
+{
+
+}
+</style>
