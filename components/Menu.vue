cec730fe895c9080d3e3fc39f8b302b6a479056e
Hynn <hynn@live.jp>
2020-12-15 17:44:15 -0300

    Alterando a Index - Adicionado logo e menu

--- components/Menu.vue
+++ components/Menu.vue
@@ -1,13 +1,3 @@
 <template>
-	<div class="dropdown"><span>teste</span>
-		<button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-			Dropdown
-		</button>
-		<div class="dropdown-menu" aria-labelledby="dropdownMenu2">
-			<button class="dropdown-item" type="button">Action</button>
-			<button class="dropdown-item" type="button">Another action</button>
-			<button class="dropdown-item" type="button">Something else here</button>
-		</div>
-	</div>
-
+	<span>teste</span>
 </template>
