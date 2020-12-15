fd807b8b81bada0f7ee4140475a459d76d6f18fb
Hynn <hynn@live.jp>
2020-12-15 18:56:11 -0300

    Alterando a Index - Ajustando o rodapé

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,5 +1,19 @@
 <template>
-	<div class="mt-3 text-white text-center">
+	<footer class="mt-3 text-white text-center">
 		Todos os direitos reservados ©
-	</div>
+	</footer>
 </template>
+
+<style>
+footer
+{
+	font-family: Oswald, serif;
+	-webkit-text-fill-color: rgb(255, 255, 255);
+	font-size: 11px;
+	letter-spacing: 1px;
+	line-height: 1.2;
+	text-transform: uppercase;
+	font-weight: 200;
+	font-style: normal;
+}
+</style>
